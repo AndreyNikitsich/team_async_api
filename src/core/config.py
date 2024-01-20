4c36a82dead40842b72a57e78b81057db1263168
Andrey Nikitsich <andrey.nikitsich@gmail.com>
2024-01-20 22:19:44 +0300

    specify rules for ruff, fix errors

--- src/core/config.py
+++ src/core/config.py
@@ -1,10 +1,10 @@
 import os
 from logging import config as logging_config
 
-from pydantic import HttpUrl, Field
+from pydantic import Field, HttpUrl
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-from core.logger import LOGGING
+from .logger import LOGGING
 
 # Применяем настройки логирования
 logging_config.dictConfig(LOGGING)
@@ -14,16 +14,16 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 class SwaggerSettings(BaseSettings):
-    PROJECT_NAME: str = Field(default='movies')
+    PROJECT_NAME: str = Field(default="movies")
 
 
 class ServicesSettings(BaseSettings):
-    REDIS_HOST: str = Field(default='127.0.0.1')
+    REDIS_HOST: str = Field(default="127.0.0.1")
     REDIS_PORT: int = Field(default=6379)
-    ES_DSN: HttpUrl = Field(default='http://127.0.0.1:9200')
+    ES_DSN: HttpUrl = Field(default="http://127.0.0.1:9200")
 
     model_config = SettingsConfigDict(
-        env_file='.env',
-        env_file_encoding='utf-8',
-        extra='ignore'
+        env_file=".env",
+        env_file_encoding="utf-8",
+        extra="ignore"
     )
