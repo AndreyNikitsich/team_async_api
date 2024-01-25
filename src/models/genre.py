8971e304cc7dd65953efb10bdb34ed20533e9da2
vladmeh <vladmeh@gmail.com>
2024-01-25 12:21:57 +0300

    Fix Genre model.

--- src/models/genre.py
+++ src/models/genre.py
@@ -2,11 +2,11 @@ from pydantic import BaseModel, Field
 
 
 class GenreFilm(BaseModel):
-    id: int
+    id: str
     title: str
     description: str | None = Field("")
 
 
 class Genre(BaseModel):
-    id: int
+    id: str
     name: str
