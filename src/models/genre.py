4232ac6724bd2d07d0105c69933357190ec537d0
vladmeh <vladmeh@gmail.com>
2024-01-28 20:20:32 +0300

    Update model && service Genres.

--- src/models/genre.py
+++ src/models/genre.py
@@ -1,12 +1,7 @@
 from pydantic import BaseModel, Field
 
 
-class GenreFilm(BaseModel):
-    id: str
-    title: str
-    description: str | None = Field("")
-
-
 class Genre(BaseModel):
     id: str
     name: str
+    description: str | None = Field("")
