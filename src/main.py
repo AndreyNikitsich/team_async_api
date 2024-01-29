8b4819eabf09246dfc4f4daa877ac21dfb6edd5a
Andrey Nikitsich <andrey.nikitsich@gmail.com>
2024-01-29 15:03:35 +0300

    use new style for lifecycle events

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+from contextlib import asynccontextmanager
+
 import uvicorn
 from api import router as api_router
 from core.config import settings
@@ -6,23 +8,24 @@ from elasticsearch import AsyncElasticsearch
 from fastapi import FastAPI
 from redis.asyncio import Redis
 
+
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    redis.redis = Redis(host=settings.redis.REDIS_HOST, port=settings.redis.REDIS_PORT)
+    elastic.es = AsyncElasticsearch(hosts=[f"{settings.es.ES_SCHEMA}://{settings.es.ES_HOST}:{settings.es.ES_PORT}"])
+    yield
+    await redis.redis.close()
+    await elastic.es.close()
+
+
 app = FastAPI(
-    title=settings.swagger.PROJECT_NAME, docs_url=settings.swagger.DOCS_URL, openapi_url=settings.swagger.OPENAPI_URL
+    lifespan=lifespan,
+    title=settings.swagger.PROJECT_NAME,
+    docs_url=settings.swagger.DOCS_URL,
+    openapi_url=settings.swagger.OPENAPI_URL,
 )
 
 app.include_router(api_router, prefix="/api")
-
-
-@app.on_event("startup")
-async def startup():
-    redis.redis = Redis(host=settings.redis.REDIS_HOST, port=settings.redis.REDIS_PORT)
-    elastic.es = AsyncElasticsearch(hosts=[f"{settings.es.ES_SCHEMA}://{settings.es.ES_HOST}:{settings.es.ES_PORT}"])
-
-
-@app.on_event("shutdown")
-async def shutdown():
-    await redis.redis.close()
-    await elastic.es.close()
 
 
 if __name__ == "__main__":
