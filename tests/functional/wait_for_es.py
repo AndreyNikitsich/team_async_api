5e4a4e3117d0d7d28fbf9b85a2ee3ffe5c7b096b
Andrey Nikitsich <andrey.nikitsich@gmail.com>
2024-02-12 00:48:16 +0300

    fix ruff

--- tests/functional/wait_for_es.py
+++ tests/functional/wait_for_es.py
@@ -1,9 +1,10 @@
 import time
 
 from elasticsearch import Elasticsearch
+
 from settings import test_settings
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     es_client = Elasticsearch(hosts=test_settings.es_url)
     while True:
         if es_client.ping():
