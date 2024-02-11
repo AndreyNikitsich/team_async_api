0c5b7df80b50e5cd1d07805d440c3cd5fd0d3b35
Andrey Nikitsich <andrey.nikitsich@gmail.com>
2024-02-11 23:40:29 +0300

    use waiters instead of docker-compose health check

--- tests/functional/settings.py
+++ tests/functional/settings.py
@@ -3,7 +3,7 @@ from typing import Any
 from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-from .testdata import es_mapping
+from testdata import es_mapping
 
 
 class TestSettings(BaseSettings):
---
+++ tests/functional/wait_for_es.py
@@ -0,0 +1,11 @@
+import time
+
+from elasticsearch import Elasticsearch
+from settings import test_settings
+
+if __name__ == '__main__':
+    es_client = Elasticsearch(hosts=test_settings.ES_URL)
+    while True:
+        if es_client.ping():
+            break
+        time.sleep(1)
